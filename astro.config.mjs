8c642edd0954c0d3f7f20ca939dab9e1bfc88639
nous-adg <alessandro@nous.cr>
2026-01-04 16:21:05 -0600

    agregando doamain

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
         plugins: [tailwindcss()]
     },
 
-    integrations: [react(), clerk()],
+    integrations: [react(), clerk({
+        domain: "https://admin.nous.cr",
+        isSatellite: true,
+    })],
     adapter: vercel()
 });
