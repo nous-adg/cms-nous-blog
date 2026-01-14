f831e709f067647dca062e6ff9d4f0609b6207a1
nous-adg <alessandro@nous.cr>
2026-01-14 15:28:58 -0600

    cambiando el favicon

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,5 +0,0 @@
-import { c, a } from "./chunks/_@astrojs-ssr-adapter_Bt9qrC0D.mjs";
-export {
-  c as createExports,
-  a as start
-};
--- .vercel/output/functions/_render.func/dist/server/pages/_image.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { a } from "../chunks/generic_BLv58g3T.mjs";
-import { renderers } from "../renderers.mjs";
-export {
-  a as page,
-  renderers
-};
